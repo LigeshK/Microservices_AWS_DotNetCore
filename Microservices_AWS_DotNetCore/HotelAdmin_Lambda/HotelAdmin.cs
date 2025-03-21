5f3fb33b09596b8a9a62dc96da07ee4b5c6e09d0
Ligesh <ligeshkoshy@gmail.com>
2025-03-21 16:25:21 +1100

    Store hotel file in S3 and save hotel details in DynamoDB using Lambda, with region and bucket from environment variables

--- Microservices_AWS_DotNetCore/HotelAdmin_Lambda/HotelAdmin.cs
+++ Microservices_AWS_DotNetCore/HotelAdmin_Lambda/HotelAdmin.cs
@@ -1,5 +1,10 @@
-﻿using Amazon.Lambda.APIGatewayEvents;
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Amazon.S3;
+using HotelAdmin_Lambda.Models;
 using HttpMultipartParser;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net;
@@ -11,7 +16,7 @@ namespace HotelAdmin_Lambda;
 
 public class HotelAdmin
 {
-    public APIGatewayProxyResponse AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
+    public async Task<APIGatewayProxyResponse> AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
     {
         // Initialize response object with default headers and a status code of 200 (OK)
         var response = new APIGatewayProxyResponse
@@ -40,6 +45,10 @@ public class HotelAdmin
         var file = formData.Files.FirstOrDefault();
         var fileName = file.FileName;
 
+        //Convert file to memorystream
+        await using var fileContentStream = new MemoryStream();
+        await file.Data.CopyToAsync(fileContentStream);
+        fileContentStream.Position = 0;
 
         var userId = formData.GetParameterValue("userId");
         var idToken = formData.GetParameterValue("idToken");
@@ -54,6 +63,42 @@ public class HotelAdmin
             // If the user is not an admin, respond with Unauthorized (401)
             response.StatusCode = (int)HttpStatusCode.Unauthorized;
             response.Body = JsonSerializer.Serialize(new { Error = "Unauthorised. Must be a member of Admin group." });
+        }
+        //Default environment varaible provided by AWS that gives region value
+        var region = Environment.GetEnvironmentVariable("AWS_Region");
+        //Add environment variable for bucket name in Lambda --> Configuration --> Environment variable
+        var bucketname = Environment.GetEnvironmentVariable("HotelBucketName");
+        //Region where lambda is executed
+        var s3client = new AmazonS3Client(RegionEndpoint.GetBySystemName(region));
+        var dbClient = new AmazonDynamoDBClient(RegionEndpoint.GetBySystemName(region));
+
+        try
+        {
+            await s3client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest
+            {
+                BucketName = bucketname,
+                Key = fileName,
+                InputStream = fileContentStream,
+                AutoCloseStream = true
+            });
+            var hotel = new Hotel
+            {
+                UserId = userId,
+                Id = Guid.NewGuid().ToString(),
+                Name = hotelName,
+                CityName = hotelCity,
+                Price = int.Parse(hotelPrice),
+                Rating = int.Parse(hotelRating),
+                FileName = fileName
+            };
+
+            using var dbContext = new DynamoDBContext(dbClient);
+            await dbContext.SaveAsync(hotel);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
         }
 
         Console.WriteLine("OK");
---
+++ Microservices_AWS_DotNetCore/HotelAdmin_Lambda/Models/Hotel.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace HotelAdmin_Lambda.Models;
+
+// Specifies that this class maps to the "Hotels" table in DynamoDB
+[DynamoDBTable("Hotels")]
+public class Hotel
+{
+    // Defines the partition key (hash key) for the DynamoDB table
+    [DynamoDBHashKey("userId")]
+    public string? UserId { get; set; }
+
+    // Defines the sort key (range key) for the DynamoDB table
+    [DynamoDBRangeKey("Id")]
+    public string? Id { get; set; }
+
+    // Hotel name
+    public string? Name { get; set; }
+
+    // Price per night (or relevant pricing metric)
+    public int Price { get; set; }
+
+    // Rating of the hotel (e.g., out of 5 stars)
+    public int Rating { get; set; }
+
+    // Name of the city where the hotel is located
+    public string? CityName { get; set; }
+
+    // File name (possibly for storing an image or related document)
+    public string? FileName { get; set; }
+}
